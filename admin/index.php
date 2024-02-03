9b46593e7436fffc95b02e09e618e3a5a8913cc8
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2024-02-04 05:41:15 +0800

    Delete admin directory

--- admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../includes.php";
-include '../partials/header-admin.php';
-?>
-
-<h1>Administration</h1>
-
-<?php include '../partials/display-movies.php'; ?>
-<?php include '../partials/footer.php'; ?>
--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-$auth = 0;
-include '../includes.php';
-$_SESSION = array();
-header('Location:'.WEBROOT.'index.php');
